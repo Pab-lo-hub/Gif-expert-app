fe610d2f67ba1d15ec1916b2b899ae55547bc8ca
pab-lo-hub <salaberry.pablo@gmail.com>
2022-11-17 20:22:08 -0300

    new details added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState([ 'One Punch', 'Metal Gear' ]);
+
+    const onAddCategory = () => {
+
+    }
+
+    console.log(categories);
+
     return (
         <>
+            {/* titulo */}
             <h1>Gif Expert App</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <button>Agregar</button>
+            <ol>
+                { categories.map( category => {
+                    return <li key={category}>{ category }</li>
+                }) }
+            </ol>
+                {/* Gif Item */}
         </>
     )
 }
