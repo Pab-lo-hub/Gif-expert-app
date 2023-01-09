426a21093467763099c5bea2f2763daa01f4ea93
pab-lo-hub <salaberry.pablo@gmail.com>
2023-01-09 17:52:52 -0300

    new function for gifs search

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,18 @@
 export const GifGrid = ({ category }) => {
+    const getGifs = async() => {
+        const url = `http://api.giphy.com/v1/gifs/search?api_key=3CNTrQTXx53hj8qx2wJK8pAikIuZKfhR&q=${ category }&limit=20`
+        const resp = await fetch( url );
+        const { data } = await resp.json();
+        const gifs = data.map( img =>({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }))
+
+        console.log(gifs);
+    }
+    getGifs();
+
 
     return (
         <>
