a8f940b0b2280e9d5a9c7fab8988b345b21cdd1d
pab-lo-hub <salaberry.pablo@gmail.com>
2023-01-13 16:03:10 -0300

    Final details

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,27 +2,24 @@ import { GifItem } from './gifitem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
+    const { images, isLoading } = useFetchGifs(category);
 
-    const { images, isLoading } = useFetchGifs( category );
-    
     return (
         <>
-            <h3>{ category }</h3>
+            <h3>{category}</h3>
             {
-                isLoading && ( <h2>Cargando...</h2> )
+                isLoading && (<h2>Cargando...</h2>)
             }
-            
 
             <div className="card-grid">
                 {
-                    images.map( ( image ) => (
-                        <GifItem 
-                            key={ image.id } 
-                            { ...image }
+                    images.map((image) => (
+                        <GifItem
+                            key={image.id}
+                            {...image}
                         />
                     ))
                 }
-                
             </div>
 
         </>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=3CNTrQTXx53hj8qx2wJK8pAikIuZKfhR&q=${ category }&limit=20`
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=3CNTrQTXx53hj8qx2wJK8pAikIuZKfhR&q=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
 
